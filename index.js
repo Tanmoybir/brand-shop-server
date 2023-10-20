c8be73a55dcc6f1ad8383285ae2bab8605550d08
Tanmoy Bir <tanmoy7468@gmail.com>
2023-10-20 17:54:56 +0600

    set up process.env file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require ('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,12 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// brand-shop
-// gfT4HCQB7QXWGxsV
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xmlybhe.mongodb.net/?retryWrites=true&w=majority`;
 
-
-const uri = "mongodb+srv://brand-shop:gfT4HCQB7QXWGxsV@cluster0.xmlybhe.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
